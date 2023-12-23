afeab473ea71cef9c542d2401a0a46c062b42ba7
unknown <chrisppoyoumbissi39@gmail.com>
2023-12-23 10:35:53 +0100

    none

--- src/Composants/Commande/Commande.jsx
+++ src/Composants/Commande/Commande.jsx
@@ -99,7 +99,7 @@ export default function Commande(props) {
     const autre  = {designation: '', prix: ''};
     const assuranceDefaut = 'aucune';
 
-    const date_e = new Date('2023-12-19');
+    const date_e = new Date('2024-12-19');
     const date_j = new Date();
 
     const [listeMedoc, setListeMedoc] = useState([]);
