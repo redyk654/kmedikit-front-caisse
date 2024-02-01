eeaac4a2e02ed3ef31488c1713a4edd9fcd30d60
unknown <chrisppoyoumbissi39@gmail.com>
2024-02-01 13:11:41 +0100

    philmedical-labo v1.0.0

--- src/shared/Globals.jsx
+++ src/shared/Globals.jsx
@@ -250,12 +250,12 @@ const hdmbanga = 'http://serveur/hdmbanga/';
 const serveurLocal = 'http://localhost:3010';
 const serveur = 'http://serveur:3010';
 
-export const liensPhilmedical = {
-    acceuil: 'http://localhost/philmedical/acceuil',
-}
-
-export const nomDns = backendLocal;
+export const nomDns = hdmbanga;
 export const nomServeurNode = serveur;
+
+export const liensPhilmedical = {
+    acceuil: 'http://serveur/philmedical/acceuil',
+}
 
 export const CATEGORIES = ["MATERNITE", "CHIRURGIE", "LABORATOIRE", "MEDECINE", "CARNET",
                             "CONSULTATION", "ECHOGRAPHIE", "MORGUE", "PEDIATRIE", "KIT", "CONSULTATION SPÉCIALISTE"]
