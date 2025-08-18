9df2ce1dfdb05390863146dab35ed23fca46b2ad
redyk654 <chrisppoyoumbissi39@gmail.com>
2025-08-18 06:48:08 +0100

    prod for hdm

--- src/Composants/Etats/ImprimerEtat.jsx
+++ src/Composants/Etats/ImprimerEtat.jsx
@@ -54,8 +54,8 @@ export default class ImprimerEtat extends Component {
         return (
             <div style={{backgroundColor: '#f1f1f1', height: '100vh', marginTop: '0px'}}>
                 <div className='w-75 m-auto'>
-                    {/* <EnteteHdm /> */}
-                    <EnteteHopital />
+                    <EnteteHdm />
+                    {/* <EnteteHopital /> */}
                 </div>
                     <div style={{fontSize: 9, display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '10px',}}>
                         <div style={{textAlign: 'center', width: '410px'}}>
